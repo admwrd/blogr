b59beb7c3fee296bf3fa9008d00d6251e4c02bc9
Andrew Prindle <prindle.andrew@gmail.com>
2017-12-01 08:48:48 -0600

    Cleaned up collate module, removed comments and unnecessary variables.  Also removed the regular expression (regex) for parsing the query string which was broken anyways.

--- src/collate.rs
+++ src/collate.rs
@@ -79,7 +79,6 @@ impl<T: Collate> Page<T> {
     /// If there are 5 items per page and the current page is 3
     /// then the start() would return 10
     pub fn start(&self) -> u32 {
-        // (cur_page - 1) * ipp
         (self.cur_page - 1) * (self.settings.ipp() as u32)
     }
     
@@ -87,7 +86,6 @@ impl<T: Collate> Page<T> {
     /// If there are 5 items per page and the current page is 3
     /// then the start() would return 14
     pub fn end(&self) -> u32 {
-        // (cur_page * ipp) - 1
         (self.cur_page * (self.settings.ipp() as u32)) - 1
     }
     
@@ -113,7 +111,6 @@ impl<T: Collate> Page<T> {
             qrystr.push_str(" LIMIT ");
             qrystr.push_str( &self.settings.ipp().to_string() );
             qrystr.push_str(" OFFSET ");
-            // qrystr.push_str(&( self.cur_page * (self.settings.ipp()-1) as u32 ).to_string());
             qrystr.push_str(&( self.start() ).to_string());
         } else {
             qrystr = String::with_capacity(query.len() + 20 + 20);
@@ -121,20 +118,14 @@ impl<T: Collate> Page<T> {
             qrystr.push_str(" LIMIT ");
             qrystr.push_str( &self.settings.ipp().to_string() );
             qrystr.push_str(" OFFSET ");
-            // qrystr.push_str(&( self.cur_page * (self.settings.ipp()-1) as u32 ).to_string());
             qrystr.push_str(&( self.start() ).to_string());
         }
-        // let mut qrystr = String::with_capacity(qrystr.len() +  );
         
         qrystr
     }
     
     pub fn navigation(&self, total_items: u32) -> String {
-        // self.settings
         // <a href="{base}{route}[?[page=x][[&]ipp=y]]">{page}</a>
-        // let something = T::default_ipp();
-        
-        // 50/20 = 2
         let ipp = self.settings.ipp() as u32;
         // integer division rounds towards zero, so if it does not evenly divide add 1
         let num_pages = if total_items % ipp != 0 {
@@ -144,25 +135,12 @@ impl<T: Collate> Page<T> {
         };
         
         if num_pages == 1 {
-            // return T::link(&self, 1);
             return link(&self, 1, "1");
         }
         
-        
-        // 4       8
-        // 0100 -> 1000
-        //
-        
-        // let abs = T::abs_links();
-        // let rel = T::rel_links();
-        // let links_min = abs as u32 + abs as u32 + rel as u32 + rel as u32 + 1u32;
         let abs = T::abs_links() as u32;
         let rel = T::rel_links() as u32;
         let links_min = abs + rel;
-        
-        // let links_total = (abs << 2) + (rel << 2) + 1;
-        // let links_total_original = abs + abs + rel + rel + 1;
-        // assert_eq!(links_total, links_total_original);
         
         let cur = if self.cur_page > num_pages { 
             num_pages 
@@ -172,24 +150,6 @@ impl<T: Collate> Page<T> {
             self.cur_page
         };
         
-        // abs ... rel cur rel ... abs
-        // add padding
-        
-        // rel left items
-        let mut rel_left = rel;
-        let mut rel_right = rel;
-        let mut abs_left = abs;
-        let mut abs_right = abs;
-        
-        let mut max = if abs > rel { abs } else { rel };
-        
-        // 1 2 3     4 5 6 7 8  9  10 11 12 13 14 15 16 17 18 19 20
-        // 1 2 3 ...     6 7 8 [9] 10 11 12            ... 18 19 20
-        
-        // 1 2 3 4 5 6 7
-        // 1 2 3 4 5 6 7
-        let first_page = link(&self, 1, "First Page");
-        
         let mut pages_left: Vec<u32> = Vec::new();
         let mut pages_right: Vec<u32> = Vec::new();
         let mut front: (Vec<u32>, Vec<u32>) = (Vec::new(), Vec::new());
@@ -198,39 +158,18 @@ impl<T: Collate> Page<T> {
         
         // print left side
         if cur <= links_min || num_pages <= links_min {
-            // if links_min > num_pages {
-            //     pages_left = (1..num_pages).collect();
-            //     // prints everything but last page, which is taken care of in the middle
-            //     // pages_left = (1..num_pages).map(|p| p ).collect();
-            // } else {
-            //     // pages_left = (1..cur).map(|p| p).collect();
             pages_left = (1..cur).collect();
-            // }
         } else {
-            // 1.. 3[4]    7.. 9[10]
-            // 1 2 3 4 5 6 7 8 9 [10] 11 12 13 14 15 16 17 18 19 20
-            // pages_left = (1..abs+1)
-            //     // .map(|p| p)
-            //     .chain((cur-rel)..cur)
-            //     .collect();
             front = ( (1..abs+1).collect(), ((cur-rel)..cur).collect() );
         }
         
-        // 1 2 3 4 5 6 7 8 9 [10] 11 12 13 14 15 16 17 18 19 20
         // print right side
-        // if cur >= (num_pages-)
         let right = num_pages - cur;
         if right <= links_min {
             pages_right = (cur+1..num_pages+1).collect();
         } else {
-            // pages_right = (cur+1..rel+1)
-            //     .chain((num_pages-abs+1)..num_pages+1)
             back = ( (cur+1..cur+rel+1).collect(), ((num_pages-abs+1)..num_pages+1).collect() );
         }
-        
-        
-        
-        
         
         // count links
         let count_left = if pages_left.len() != 0 {
@@ -244,27 +183,23 @@ impl<T: Collate> Page<T> {
             back.0.len() + back.1.len()
         };
         
-        // <a href="">10</a> + http://localhost:8000/pagination?page=10&ipp=20
-        // 65 characters roughly for each link
-        // Add 70 chars per link, plus 150 for previous/next links, plus 70 for the 
-        // current link plus buffer of 100 characters to avoid extra allocations
         let mut html: String = String::with_capacity((count_left + count_right) * 70 + 150 +70 + 100);
         
-        println!(r"Pagination Debug:
-            cur: {}, ipp: {}, num_pages: {},
-            abs: {}, rel: {}, links_min: {},
-            pages_left: {:?},
-            front: {:?},
-            pages_right: {:?}
-            back: {:?}
-            ", 
-            cur, ipp, num_pages, 
-            abs, rel, links_min,
-            pages_left,
-            front,
-            pages_right,
-            back
-            );
+        // println!(r"Pagination Debug:
+        //     cur: {}, ipp: {}, num_pages: {},
+        //     abs: {}, rel: {}, links_min: {},
+        //     pages_left: {:?},
+        //     front: {:?},
+        //     pages_right: {:?}
+        //     back: {:?}
+        //     ", 
+        //     cur, ipp, num_pages, 
+        //     abs, rel, links_min,
+        //     pages_left,
+        //     front,
+        //     pages_right,
+        //     back
+        //     );
         
         
         if cur != 1 {
@@ -275,8 +210,6 @@ impl<T: Collate> Page<T> {
             for page in pages_left {
                 html.push_str( &link(&self, page, &page.to_string()) );
             }
-            // print previous page link
-            // print link to all pages in the vector
         } else if front.0.len() !=0 || front.1.len() != 0 {
             for page in front.0 {
                 html.push_str( &link(&self, page, &page.to_string()) );
@@ -311,115 +244,6 @@ impl<T: Collate> Page<T> {
         
         html.shrink_to_fit();
         html
-        
-        
-        
-        
-        // if cur_page != 1 {
-        //     // display first link
-        //     let url = T::link(self, cur_page-1);
-        //     // <a href="">Previous</a>
-        //     let mut link = String::with_capacity(url.len() + 23 + 10);
-            
-        // }
-        
-        
-        // only add ... if there is a gap of more than {padding} pages
-        // keep the +1 ???
-        // if cur <= rel+1 {
-        // if cur <= rel {
-        //     let start_front = cur - 1;  // TODO - fix this equation
-        //     let start_back = rel - cur; // TODO - fix this equation
-        //     // let end = num_pages - rel + 1; // Removed - forgetting absolutes for now
-            
-            
-        // } else if cur >= (num_pages-rel) {
-        //     let end_front = ;
-        //     let end_back = ;
-        //     // let start = ; // Removed - forgetting absolutes for now
-            
-            
-            
-        // } else {
-        //     // first page ... rels page 
-        //     // 
-            
-        // }
-        
-        // if cur_page != num_pages && num_pages > 1 {
-        //     // display next link
-        // }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // if cur < max {
-        //     rel_front = 
-        //     rel_left = max - cur;
-        //     abs_left = 0;
-        // } else {
-        //     if cur > abs {
-        //         rel_left = rel;
-                
-        //     } else {
-                
-        //     }
-        // }
-        
-        
-        
-        
-        
-        
-        // let padding = T::links_padding();
-        // // add lenience to num_pages so that if there is ONE (or so) more page
-        // // more than the links_min it still shows all the pages.
-        // if num_pages + padding <= links_min {
-        //     if cur == 1 {
-                
-        //     } else if cur < abs {
-                 
-                
-                
-        //     } else if cur == num_pages {
-                
-        //     } else if cur > (num_pages - max) {
-                
-        //     }
-            
-        //     // show all links between start and end
-        //     // highlight current link
-        // } else if self.cur_page < abs {
-        //     // print first abs and last abs
-        //     // current page should fall in the first abs, highlight it
-            
-        // } else if self.cur_page >= num_pages - abs {
-        //     // print first abs and last abs
-        //     // current page should fall in the last abs, highlight it
-            
-        // } else if  {
-        //     // print first abs pages
-        //     // print rel links before and after
-        //     // print last abs pages
-        // }
-        
-        
-        
-        // if self.cur_page < self.settings.abs_links() {
-        //     if 
-        // }
-        
-        // String::new()
     }
 }
 
@@ -476,27 +300,12 @@ pub trait Collate {
             if chunks.len() != 2 { continue; }
             let key = chunks[0];
             let value = chunks[1];
-            // for (key, value) in  {
-            // for &(key, value) in pair.splitn(2, '=').collect::<Vec<&str>>() {
                 match key {
                     "page" => { cur_page = value.parse().unwrap_or(1); },
                     "ipp" => { ipp = value.parse().unwrap_or(Self::default_ipp()); },
                     _ => {},
                 }
-            // }
         }
-        
-        // lazy_static! {
-        //     static ref PARSE_QUERYSTRING: Regex = Regex::new(r#"^(?page=(?P<page>\d+))?&?(?:ipp=(?P<ipp>\d+))$"#).unwrap();
-        // }
-        
-        // for cap in PARSE_QUERYSTRING.captures(qrystr) {
-        //     if let Some(pg) = cap.name("page") {
-        //         cur_page = pg.as_str().parse().unwrap_or(1);
-        //     } else if let Some(ip) = cap.name("ipp") {
-        //         ipp = ip.as_str().parse().unwrap_or(Self::default_ipp());
-        //     }
-        // }
         
         Page {
             cur_page,
@@ -531,8 +340,6 @@ impl<'a, 'r, T: Collate> FromRequest<'a, 'r> for Page<T> {
                     settings: T::new(T::default_ipp()),
                 })
             }
-            // Outcome::Success( page )
-            // Outcome::Forward(())
     }
 }
 
