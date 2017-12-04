dc848530e4c2d30fd907b40270e243167e8b315b
Andrew Prindle <prindle.andrew@gmail.com>
2017-12-03 23:15:22 -0600

    Made search button hidden unless the search field has focus.

--- static/blog_after.js
+++ static/blog_after.js
@@ -1,5 +1,35 @@
 
 
+
+// var searchform = document.getElementById('search-form').element["q"].style.display = 'none';
+
+
+var searchfield = document.getElementById('search-form').q;
+var searchbtn = document.getElementById('search-form').lastElementChild;
+if (searchfield && searchbtn) {
+    hide_search();
+    searchfield.addEventListener('change', function() { show_search(); });
+    searchfield.addEventListener('focus', function() { show_search(); });
+    searchfield.addEventListener('blur', function() { hide_search(); });
+} else {
+    console.log("No search field or button");
+}
+
+function hide_search() {
+    // commented out code to make the search button stay visible if
+    //     there is text in the search field
+    // if (searchfield.value == "") {
+        searchbtn.style.display = 'none';
+    // } else {
+        // searchbtn.style.display = 'block';
+    // }
+}
+
+function show_search() {
+    searchbtn.style.display = 'block';
+}
+
+
 // var tag_form = document.forms.insert_form.elements["tags"];
 var tag_form = document.getElementById("insert-tags");
 if (tag_form) {
