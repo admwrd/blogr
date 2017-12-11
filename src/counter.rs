fa54c7429e150f4a6a0aba35c7913801a734b2f4
Andrew Prindle <prindle.andrew@gmail.com>
2017-12-11 04:50:10 -0600

    Adding loading/storing to PageCount and ViewsTotal, just need to test it.

--- src/counter.rs
+++ src/counter.rs
@@ -8,13 +8,16 @@ use rocket::response::{self, Response, content, NamedFile, Redirect, Flash, Resp
 use rocket::response::content::Html;
 use rocket::State;
 
+
 use std::mem;
+use std::env;
 use std::collections::HashMap;
 use std::ffi::OsStr;
-use std::io::{Cursor, Read};
+use std::io::prelude::*;
+use std::io::{Cursor, Read, Write};
 use std::path::{Path, PathBuf};
 use std::fs::{self, File};
-use std::io::prelude::*;
+use ::serde::{Deserialize, Serialize};
 
 use std::sync::{Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -28,25 +31,62 @@ use std::net::Ipv4Addr;
 
 
 
+
+pub fn cur_dir_file(name: &str) -> PathBuf {
+    if let Ok(mut dir) = env::current_exe() {
+        dir.pop();
+        println!("Climbing directory tree into: {}", &dir.display());
+        dir.pop();
+        println!("Loading into directory: {}", &dir.display());
+        dir.set_file_name(name);
+        println!("Load file is: {}", &dir.display());
+        dir
+    } else {
+        PathBuf::from(name)
+    }
+}
+
+
+
+#[derive(Debug)]
 pub struct ViewsTotal(pub AtomicUsize);
 
 impl ViewsTotal {
     pub fn new() -> ViewsTotal {
         ViewsTotal(AtomicUsize::new(0))
     }
+    pub fn load() -> ViewsTotal {
+        let filename = cur_dir_file("views_total.json");
+        // let mut f = File::open(filename).expect("Could not load ViewsTotal file.");
+        let mut f_rst = File::open(&filename);
+        if let Ok(mut f) = f_rst {
+            let mut buf: String = String::with_capacity(50);
+            f.read_to_string(&mut buf);
+            let des: usize = ::serde_json::from_str(&mut buf).unwrap_or(0);
+            
+            ViewsTotal( AtomicUsize::new(des) )
+        } else {
+            if let Ok(mut f) = File::create(&filename) {
+                let bytes = f.write(0.to_string().as_bytes());
+                ViewsTotal( AtomicUsize::new(0) )
+            } else {
+                ViewsTotal( AtomicUsize::new(0) )
+            }
+        }
+    }
+    pub fn save(views: usize) {
+        let filename = cur_dir_file("views_total.json");
+        let mut f = File::create(&filename).expect("Could not create ViewsTotal file.");
+        
+        let ser: String = ::serde_json::to_string_pretty(&views).expect("Could not serialize ViewsTotal.");
+        println!("Saving ViewsTotal to: {}.  Data: {}", filename.display(), ser);
+        let bytes = f.write(ser.as_bytes());
+    }
 }
 
-// impl<'a, 'r> FromRequest<'a, 'r> for ViewsTotal {
-//     type Error = ();
-    
-//     // fn from_request(req: &'a Request<'r>) -> ::rocket::request::Outcome<PageCount,Self::Error>{
-//     fn from_request(req: &'a Request<'r>) -> ::rocket::request::Outcome<ViewsTotal,Self::Error>{
-        
-//     }
-// }
 
 
-
+#[derive(Debug, Serialize, Deserialize)]
 pub struct PageCount {
     pub count: Mutex<HashMap<String, usize>>,
 }
@@ -57,10 +97,43 @@ impl PageCount {
             count: Mutex::new(HashMap::new()),
         }
     }
+    pub fn load() -> PageCount {
+        let filename = cur_dir_file("page_count.json");
+        // let mut f = File::open(filename).expect("Could not load ViewsTotal file.");
+        let mut f_rst = File::open(&filename);
+        if let Ok(mut f) = f_rst {
+            let mut buf: String = String::with_capacity(1500);
+            f.read_to_string(&mut buf);
+            let des: PageCount = ::serde_json::from_str(&mut buf).unwrap_or( PageCount::new() );
+            des
+            
+            // ViewsTotal( AtomicUsize::new(des) )
+        } else {
+            if let Ok(mut f) = File::create(&filename) {
+                let new = PageCount::new();
+                new.save();
+                new
+                // let mut buf: String = String::with_capacity(50);
+                // les des: PageCount = serde_json::from_str(&mut)
+                // let bytes = f.write("".to_string().as_bytes());
+            } else {
+                PageCount::new()
+            }
+        }
+    }
+    pub fn save(&self) {
+        let filename = cur_dir_file("page_count.json");
+        let mut f = File::create(&filename).expect("Could not create PageCount file.");
+        
+        let ser: String = ::serde_json::to_string_pretty(self).expect("Could not serialize PageCount.");
+        println!("Saving PageCount to: {}.  Data: {}", filename.display(), ser);
+        let bytes = f.write(ser.as_bytes());
+    }
 }
 
 
 // current page/route, page views, total site hits/views
+#[derive(Debug, Clone, Serialize)]
 pub struct Hits(pub String, pub usize, pub usize);
 
 
@@ -115,6 +188,11 @@ impl<'a, 'r> FromRequest<'a, 'r> for Hits {
             // *hits += 1;
             (*hits).wrapping_add(1);
             
+            // let hit = *hits;
+            // Every 100 page views save the stats
+            if (*hits) % 100usize == 0 {
+                
+            }
             
             
             Outcome::Success( Hits(pagestr, *hits, views) )
